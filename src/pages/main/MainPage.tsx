5f65feb5fd46b7e2bd4b1bf0654474a8bff1f9a3
VikaLubenets <lubenetcvika@gmail.com>
2025-06-18 11:51:57 +0200

    feat: add basic styles var and btn ui component

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,10 +1,9 @@
 function MainPage() {
-    return (
-      <div>
-        <h1>main page</h1>
-      </div>
-    );
-  }
-  
-  export default MainPage;
-  
+  return (
+    <div>
+      <h1>main page</h1>
+    </div>
+  );
+}
+
+export default MainPage;
