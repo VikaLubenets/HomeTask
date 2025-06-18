2b272924ab24b53e1e0c23e45e8111a08d0cf0d7
VikaLubenets <lubenetcvika@gmail.com>
2025-06-18 16:52:10 +0200

    feat: add header

---
+++ src/pages/AnaliticsPage/components/HighlightsInfo.tsx
@@ -0,0 +1,7 @@
+export const HighlightsInfo = () => {
+    return (
+        <section>
+            highlights
+        </section>
+    )
+}
--- src/pages/main/MainPage.tsx
+++
@@ -1,9 +0,0 @@
-function MainPage() {
-  return (
-    <div>
-      <h1>main page</h1>
-    </div>
-  );
-}
-
-export default MainPage;
